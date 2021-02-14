caa67f792bd5c544c848a4c30f18889c0fd1ce28
Dogorshom <saloom.dogorshom@gmail.com>
2021-02-14 05:34:37 +0200

    upload the php source

---
+++ test.php
@@ -0,0 +1,17 @@
+<?php
+$name="saloom";
+$age="21";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>
+<?php echo $name;?>
+</title>
+<body> 
+<h1><?php echo "My Heading";?></h1>
+<p> <?php echo $name . " " . $age?></p>
+</body>
+</head>
+</html>    
